8f7b0ab2f7693128d332545eb3a70acf64cea051
Hend <cathe.2007@gmail.com>
2015-12-01 19:45:18 +0200

    Fix multiple network states of the same type

--- app/src/main/java/com/hendalqett/products/receivers/NetworkStateReceiver.java
+++ app/src/main/java/com/hendalqett/products/receivers/NetworkStateReceiver.java
@@ -5,24 +5,44 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 import com.hendalqett.products.otto.BusProvider;
 import com.hendalqett.products.utils.NetworkStateChanged;
 
 
 public class NetworkStateReceiver extends BroadcastReceiver {
+    private static boolean firstConnect = true;
+    private static boolean firstDisconnect = true;
+
 
     public void onReceive(Context context, Intent intent) {
         if (intent.getExtras() != null) {
-            NetworkInfo ni = (NetworkInfo) intent.getExtras().get(ConnectivityManager.EXTRA_NETWORK_INFO);
-            if (ni != null && ni.getState() == NetworkInfo.State.CONNECTED) {
+            NetworkInfo networkInfo = (NetworkInfo) intent.getExtras().get(ConnectivityManager.EXTRA_NETWORK_INFO);
+            if (networkInfo != null && networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                firstDisconnect=true;
+                if (firstConnect) {
+                    BusProvider.getInstance().post(new NetworkStateChanged(true));
 
-                BusProvider.getInstance().post(new NetworkStateChanged(true));
+                    firstConnect=false;
+                }
 
-            } else
-                BusProvider.getInstance().post(new NetworkStateChanged(false));
+            }
+            else {
+                firstConnect=true;
+                if (firstDisconnect) {
+                    BusProvider.getInstance().post(new NetworkStateChanged(false));
+                    firstDisconnect=false;
+
+                }
+            }
             if (intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, Boolean.FALSE)) {
-                BusProvider.getInstance().post(new NetworkStateChanged(false));
+                firstConnect=true;
+                if (firstDisconnect) {
+                    Log.i("Adapter", "First DisConnect true");
+                    BusProvider.getInstance().post(new NetworkStateChanged(false));
+                    firstDisconnect=false;
+                }
 
             }
         }
